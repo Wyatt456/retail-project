c6d2a6b3dacf5611a9d76357da1d9f24328a553a
Chen Siyu <15301691595@163.com>
2026-03-13 17:10:18 +0800

    update comments and module

--- src/analysis/utils.py
+++ src/analysis/utils.py
@@ -4,29 +4,28 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # project root directory
-BASE_DIR = Path(__file__).resolve().parents[2]
+BASE_DIR = Path(__file__).resolve().parents[2] #resolve()获取绝对路径 parents[2]获取上两级目录
 
-DATA_PATH = BASE_DIR / "data" / "cleaned" / "retail_clean.csv"
-OUTPUT_DIR = BASE_DIR / "outputs" / "figures"
+DATA_PATH = BASE_DIR / "data" / "cleaned" / "retail_clean.csv" #数据路径 BASE_DIR/data/cleaned/retail_clean.csv
+OUTPUT_DIR = BASE_DIR / "outputs" / "figures" #输出路径 BASE_DIR/outputs/figures
 
 
-def load_data() -> pd.DataFrame:
-    df = pd.read_csv(DATA_PATH, parse_dates=["InvoiceDate"])
+def load_data() -> pd.DataFrame: #->只是一个提示符而已，告诉调用者这个函数应该返回一个DataFrame对象
+    df = pd.read_csv(DATA_PATH, parse_dates=["InvoiceDate"])#因为我们之前把xlsx文件转换成了csv（不记录文本格式），所以这里用pd.read_csv来读取数据，并且指定parse_dates参数来解析InvoiceDate列为日期格式
 
     # ensure output folder exists
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
     # clean column names if needed
-    df.columns = [col.strip() for col in df.columns]
-
+    df.columns = df.columns.str.strip() #去掉列名的空格，防止后续操作中因为列名有空格而导致错误
     return df
 
 
 def set_plot_style() -> None:
-    sns.set_theme(style="whitegrid")
-    plt.rcParams["figure.figsize"] = (12, 6)
-    plt.rcParams["axes.titlesize"] = 14
-    plt.rcParams["axes.labelsize"] = 12
-    plt.rcParams["xtick.labelsize"] = 10
-    plt.rcParams["ytick.labelsize"] = 10
-    plt.rcParams["figure.dpi"] = 120
+    sns.set_theme(style="whitegrid") #设置seaborn的主题为白色网格，这样图表看起来更清晰
+    plt.rcParams["figure.figsize"] = (12, 6) #图表大小 = 12 × 6 英寸
+    plt.rcParams["axes.titlesize"] = 14 #标题大小
+    plt.rcParams["axes.labelsize"] = 12 #坐标轴标签大小
+    plt.rcParams["xtick.labelsize"] = 10 #x轴刻度标签大小
+    plt.rcParams["ytick.labelsize"] = 10 #y轴刻度标签大小
+    plt.rcParams["figure.dpi"] = 120 #图表分辨率
--- src/download_data.py
+++ src/download_data.py
@@ -1,22 +1,21 @@
 from pathlib import Path
 import kagglehub
 import shutil
-import os
 
 # project root
-ROOT = Path(__file__).resolve().parent.parent
+ROOT = Path(__file__).resolve().parent.parent #__file__是 Python 自动提供的变量，表示：当前脚本的路径 resolve()方法将路径转换为绝对路径 parent.parent表示上两级目录，也就是项目根目录
 
 # data folder
 destination = ROOT / "data" / "raw"
 
-# download dataset
-path = kagglehub.dataset_download("nankisinghsohi/online-retail-ii-dataset")
+destination.mkdir(parents=True, exist_ok=True) #如果父目录不存在则创建父目录，如果目标文件已经存在则不报异常
 
+# download dataset
+path = Path(kagglehub.dataset_download("nankisinghsohi/online-retail-ii-dataset"))
+#类型转化为Path对象，方便后续操作
 print("Downloaded to:", path)
 
-os.makedirs(destination, exist_ok=True)
-
-for file in os.listdir(path):
-    shutil.copy(os.path.join(path, file), destination)
+for file in path.iterdir(): #遍历下载的文件夹中的所有文件
+    shutil.copy(file, destination) #shutil.copy()函数用于复制文件，第一个参数是源文件路径，第二个参数是目标文件路径，这里我们把下载的文件复制到data/raw目录下
 
 print("Dataset copied to:", destination)
